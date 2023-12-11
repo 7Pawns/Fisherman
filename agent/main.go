1b0099a0b8e4dde17852c7b3a545f27d05c014bb
Orad <oradsegal11@gmail.com>
2023-12-12 00:57:10 +0200

    getting hostname

--- agent/main.go
+++ agent/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Testing connections")
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("ShyGuy initialized on %s.", hostname)
 }
